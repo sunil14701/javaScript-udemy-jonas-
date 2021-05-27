2dc0e30c41bec2e29a7f41cd5b02aa9acfaf6916
sunil kumar <sunilkumar333221@gamil.com>
2021-05-27 23:56:50 +0530

    done

---
+++ module3/002.js
@@ -0,0 +1,14 @@
+'use strict';//block bug in our code. use it in every program
+console.log("lecture 002");//activitaing strict mode
+
+let hasDrivingLisence = false;
+const passTest = true;
+
+//use of strict mode
+// if(passTest) hasDrivinLisence = true;
+if(hasDrivingLisence) console.log("you now can drive");
+
+//use of strict mode
+// let interface = "audio";//it is a reserved word in js which can be used in js in future . so to dont use these kind of word we have to use strict mode.
+
+// let private = 675;reserved code
