b8f3989b6c81bf97ca3da69faea5fbab1b528a3a
sunil kumar <sunilkumar333221@gamil.com>
2021-05-27 23:30:06 +0530

    practice

---
+++ module2/024.js
@@ -0,0 +1,14 @@
+console.log("lecture 24");//coding challange 4 
+
+//use of if-else is probiheted
+let bill = 275;
+let tip;
+console.log(`your bill is ${bill} and your tip is ${(bill>50 && bill<300) ? (tip=bill*0.15):(tip=bill*.2) }, so your total bill is ${bill+tip}`);
+
+let bill2 = 40;
+// let tip;
+console.log(`your bill is ${bill2} and your tip is ${(bill2>50 && bill2<300) ? (tip=bill2*0.15):(tip=bill2*.2) }, so your total bill is ${bill2+tip}`);
+
+let bill3 = 430;
+// let tip;
+console.log(`your bill is ${bill3} and your tip is ${(bill3>50 && bill3<300) ? (tip=bill3*0.15):(tip=bill3*.2) }, so your total bill is ${bill3+tip}`);
