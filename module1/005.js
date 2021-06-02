e95fa5cc236267b8d8ba95d51b8518dde33950af
sunil kumar <sunilkumar333221@gamil.com>
2021-06-02 20:43:19 +0530

    added notes to folder

--- module1/005.js
+++ module1/005.js
@@ -1,3 +1,3 @@
 console.log('lecture 005');//setting up our code editor
 
-console.log("hello world");//print the value to the console of the webpage.n
+console.log("hello world");//print the value to the console of the webpage.
